6eea35687655c8a58465545a689463fdcd57e5e0
tauhidulcog <tauhidul@cogwheelanalytics.com>
2025-03-12 22:24:15 +0600

    tour guide working perfectly using ngx-ui-tour-primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,19 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import {IntroGuideService} from '../services/intro-guide.service';
 import {TodoService} from '../services/todo.service';
+import {TourPrimeNgModule} from 'ngx-ui-tour-primeng';
+import { provideAnimationsAsync} from '@angular/platform-browser/animations/async'
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+;
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), IntroGuideService, TodoService]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), IntroGuideService, TodoService, TourPrimeNgModule, provideAnimationsAsync(),
+
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+]
 };
